6a0806c3878d300d5d29ace5b4deb5675d2c8af4
JessicaOFerreira <51684387+JessicaOFerreira@users.noreply.github.com>
2021-04-11 20:52:36 -0300

    Add files via upload

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -9,7 +9,9 @@ import Mural from './pages/Mural/Mural.js';
 import Persons from './pages/Persons/Persons.js';
 import Homeworks from './pages/Homeworks/Homeworks.js';
 import Create_Homework from './pages/Create_atividade/Create_Homework.js';
-import Create_Material from './pages/Create_Material/Create_Material.js'
+import Create_Material from './pages/Create_Material/Create_Material.js';
+import Information_Homework from './pages/Information_Homeworks/Info_Home.js';
+import Information_Material from './pages/Information_Material/Info_Mate.js';
 
 export default function Routes() {
     return (
@@ -24,6 +26,9 @@ export default function Routes() {
                 <Route path="/homeworks" component={Homeworks} />
                 <Route path="/createHomework" component={Create_Homework}/>
                 <Route path="/createMaterial" component={Create_Material}/>
+                <Route path="/informationHomework" component={Information_Homework}/>
+                <Route path="/informationMaterial" component={Information_Material}/>
+
             </Switch>
         </BrowserRouter>
     )
