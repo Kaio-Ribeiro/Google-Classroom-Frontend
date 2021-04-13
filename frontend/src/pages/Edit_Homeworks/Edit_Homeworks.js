168d8650c0f13e2079970a98cf375ae740231714
antony-raul <raulzin.raul@gmail.com>
2021-04-13 18:35:51 -0300

    edit incomplete homeworks

--- frontend/src/pages/Edit_Homeworks/Edit_Homeworks.js
+++ frontend/src/pages/Edit_Homeworks/Edit_Homeworks.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRoute } from 'react'
 import {useLocation} from "react-router-dom";
 import { Link, useHistory } from 'react-router-dom';
 import api from '../../services/api';
@@ -6,10 +6,47 @@ import api from '../../services/api';
 import './Edit_Home.css'
 
 function Edit_Homework() {
+  
+  const [title,setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [fullPoints, setFullPoints] = useState('')
+  const [dateLimit, setDateLimit] = useState('')
+
   const history = useHistory()
   const location = useLocation();
   const classID = localStorage.getItem('class_id')
+  const userID = localStorage.getItem('userID')
   const homework = location.state.homework
+
+  async function handleEditHomework(event) {
+    event.preventDefault()
+
+    const data = new FormData()
+    data.append('title',title)
+    data.append('fullPoints',fullPoints)
+    data.append('description', description)
+    data.append('dateLimit',dateLimit)
+    data.append('classID', classID)
+
+    try{
+      if (title === '' || description === '') {
+          alert('Preencha todos os dados!')
+          
+      }else {
+          await api.put('/homeworks', data, {
+              headers: {
+                  Authorization: userID
+              }
+          })
+  
+          alert('Atividade editada!')
+          history.push('/informationHomework');
+      }
+
+  }catch(err) {
+      alert('Erro ao editar atividade')
+  }
+  }
 
   return (
       <div className="Body-EditHome">
@@ -24,7 +61,7 @@ function Edit_Homework() {
           </div>
 
           <div class="save-edit-atv">
-              <button class="btn-save-atvd">Salvar</button>
+              <button class="btn-save-atvd" type="submit" onClick={handleEditHomework}>Salvar</button>
           </div>
 
         </div>
@@ -34,9 +71,9 @@ function Edit_Homework() {
           <form class="form-edit-atvd">
 
             <div class="edit-info1-atvd">
-                <input type="text" id="edit-title-atvd" placeholder="Titulo" name="tAtv" value={homework.title}/> <br/>
+                <input type="text" id="edit-title-atvd" placeholder="Titulo" name="tAtv" defaultValue={homework.title} onChange={e => setTitle(e.target.value)}/> <br/>
 
-                <textarea type="text" id="edit-description-atvd" name="dAtv" placeholder="Instruções (opcional)" value={homework.description}></textarea> <br/>
+                <textarea type="text" id="edit-description-atvd" name="dAtv" placeholder="Instruções (opcional)" defaultValue={homework.description} onChange={e => setDescription(e.target.value)}></textarea> <br/>
                 
                 <div class="list-anexos-edition">
                   
@@ -49,10 +86,10 @@ function Edit_Homework() {
             <div class="edit-info2-atvd">
 
                 <label class="pont-Max-edition">Pontuação Máxima</label> <br></br>
-                <input type="text" name="pMaxAtv" id="pont-max-edition" value={homework.fullPoints}/> <br></br>
+                <input type="text" name="pMaxAtv" id="pont-max-edition" defaultValue={homework.fullPoints} onChange={e => setFullPoints(e.target.value)}/> <br></br>
 
                 <label class="data-Ent-edition">Data de Entrega</label> <br></br>
-                <input type="date" id="data-ent-edition" name="dataEntrega" value=""/>
+                <input type="date" id="data-ent-edition" name="dataEntrega" defaultValue="" onChange={e => setDateLimit(e.target.value)}/>
 
             </div>
 
